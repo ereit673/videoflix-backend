70819a994bc306fa41d4928b5bb9df5757a03f9e
Alex Eremie <146241194+ereit673@users.noreply.github.com>
2025-08-18 00:01:13 +0200

    Add HLSPlaylistView to retrieve HLS Playlist for a specific video and resolution

--- videos_app/api/urls.py
+++ videos_app/api/urls.py
@@ -3,5 +3,7 @@ from videos_app.api import views
 
 
 urlpatterns = [
-    path('video/', views.VideosListView.as_view(), name="videos-list")
+    path('video/', views.VideosListView.as_view(), name="videos-list"),
+    path('video/<int:movie_id>/<str:resolution>/index.m3u8',
+         views.HLSPlaylistView.as_view(), name="hls-playlist"),
 ]
--- videos_app/api/views.py
+++ videos_app/api/views.py
@@ -1,6 +1,9 @@
+import os
+
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-
+from django.http import FileResponse, Http404
+from django.conf import settings
 from videos_app.models import Video
 from videos_app.api.serializers import VideoSerializer
 
@@ -12,3 +15,29 @@ class VideosListView(generics.ListAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
     permission_classes = [IsAuthenticated]
+
+
+class HLSPlaylistView(generics.RetrieveAPIView):
+    """
+    View to retrieve the HLS playlist for a specific video and resolution.
+    """
+    queryset = Video.objects.all()
+    lookup_field = "id"
+    lookup_url_kwarg = "movie_id"
+    # permission_classes = [IsAuthenticated]
+
+    def retrieve(self, request, *args, **kwargs):
+        video = self.get_object()
+        resolution = kwargs.get("resolution")
+        playlist_path = os.path.join(
+            settings.MEDIA_ROOT,
+            'videos',
+            video.title,
+            resolution,
+            'index.m3u8'
+        )
+
+        if not os.path.exists(playlist_path):
+            raise Http404("Playlist not found")
+
+        return FileResponse(open(playlist_path, 'rb'), content_type='application/vnd.apple.mpegurl')
